8466ac5c72119071acf97f65c822118e46199335
RussianCabbageSoup <testylya333@gmail.com>
2026-09-08 00:13:22 +0300

    hide passed day

--- src/components/schedule/schelude.jsx
+++ src/components/schedule/schelude.jsx
@@ -1,8 +1,15 @@
 import ScheduleCard from "./scheludeCard";
 
 const Schedule = ({ data }) => {
+    const today = new Intl.DateTimeFormat('en-CA', {
+        timeZone: 'Europe/Moscow',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    }).format(new Date());
+
     const grouped = {};
-    data.forEach(item => {
+    data.filter(item => item.дата.slice(0, 10) >= today).forEach(item => {
         const key = item.дата.slice(0, 10);
         if (!grouped[key]) grouped[key] = [];
         grouped[key].push(item);
--- src/http/schedule.js
+++ src/http/schedule.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 const fetchSchedule = async () => {
     try {
-        const today = new Date().toISOString().slice(0, 10);
+        const today = new Intl.DateTimeFormat('en-CA', {
+            timeZone: 'Europe/Moscow',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        }).format(new Date());
+        console.log(today);
         const { data } = await axios.get(`https://erp.pkgh.ru/api/Rasp?idGroup=13067&sdate=${today}`);
         return data.data;
     } catch (error) {
